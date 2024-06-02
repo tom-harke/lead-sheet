3f9289fbff76e2285bc7a7059f1667db78bef43f
Tom Harke <tom.harke@gmail.com>
2024-06-01 20:53:30 -0700

    add eg of 'bookmarks'

---
+++ example.bookmark.mk
@@ -0,0 +1,14 @@
+#!/usr/bin/make -f
+
+# This is known to work
+example.pdf:
+	gs -dBATCH -dNOPAUSE -dQUIET -sDEVICE=pdfwrite -sOutputFile=example.pdf example.bookmark.toc  lesno.book.pdf
+
+# This was found in another write-up
+alternate:
+	gs -dBATCH -dNOPAUSE -sDEVICE=pdfwrite -q -sOutputFile=output.pdf -dPDFSETTINGS=/prepress index.info -f input.pdf
+
+# Ideas:
+#  - unicode support is weird -- need to use 16BE / encode as hex
+#       [/Title <FEFF0028004C00FC00670065006E006200610072006F006E0029> /Count 0 /Color 1 1 1 /Page 1 /View [/XYZ null null null]  /OUT pdfmark
+#  - use qpdf to uncompress pdf
